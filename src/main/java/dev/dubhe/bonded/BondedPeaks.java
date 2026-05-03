dfbe902232e61079dd8d71dd045085d67cb09936
Gugle <administration@ztxy666.cn>
2026-05-03 19:35:03 +0800

    Rename and refactor mod from NeoforgeTemplateMod to BondedPeaks

---
+++ src/main/java/dev/dubhe/bonded/BondedPeaks.java
@@ -0,0 +1,13 @@
+package dev.dubhe.bonded;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(BondedPeaks.MOD_ID)
+public class BondedPeaks {
+    public static final String MOD_ID = "bonded_peaks";
+
+    public BondedPeaks(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
